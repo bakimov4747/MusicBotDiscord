b1cbae463ed9f4ed6668f5df9efe7fb469abdc7e
Bobryin <bakimov4747@gmail.com>
2026-06-06 20:46:23 +0500

    Начинается переделка :)

--- src/events/ready.js
+++
@@ -1,7 +0,0 @@
-export const name = 'ready';
-export const once = true;
-
-export function execute(client) {
-  console.log(`✅ ${client.user.tag} онлайн! 🎵`);
-  client.user.setActivity('/play — включи музыку', { type: 2 });
-}
